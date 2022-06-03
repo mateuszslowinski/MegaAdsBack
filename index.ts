ab662f2ccbadc4e9c1e2a4a34e1033a131a10f40
Mateusz <nowe7246@gmail.com>
2022-06-03 16:08:10 +0200

    Added cors origin to config.

--- index.ts
+++ index.ts
@@ -4,11 +4,12 @@ import 'express-async-errors';
 import {handleError} from "./utlis/errors";
 import rateLimit from "express-rate-limit";
 import {adRouter} from "./routes/ad.router";
+import {config} from "./config/config";
 
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: config.corsOrigin,
 }));
 
 app.use(express.json());
